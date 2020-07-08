3d9995d3771d9e29a7dc68b930318c3a17f30dd5
raghadao <67149416+raghadao@users.noreply.github.com>
2020-07-09 02:30:25 +0300

    i

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -8,15 +8,19 @@
         :book='book'
       >{{book.title}}:{{book.author}}</book-item>
     </ul>
+    <hr>
+    <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 export default {
   name: 'BookList',
   components:{
-    BookItem
+    BookItem,
+    BookForm
 
   },
   data() {
@@ -28,6 +32,13 @@ export default {
         {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
       ]
     };
+  },
+  methods:{
+     appendBook(bookTitle, bookAuthor){
+       this.books.push({ title: bookTitle, author: bookAuthor})
+
+     }
+
   }
 };
 </script>
